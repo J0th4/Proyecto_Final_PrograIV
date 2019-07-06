4538975d994a4516c3cd5382861fe486859f55ed
utp <utp@192.168.9.58>
2019-07-06 12:40:17 -0500

    creacion de clases

---
+++ Gestor_de_proyectos/src/modelo/proyecto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author utp
+ */
+public class proyecto {
+    private tarea tarea;
+    private List <persona> personas; 
+    
+}
